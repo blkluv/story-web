84ee8995eb3e5cd48939f524ff1740aec9e1ad9a
blkluv <info@blkluv.org>
2023-10-11 20:02:26 -0400

    fix

--- nextjs/src/components/StorytellerStoriesGridView.tsx
+++ nextjs/src/components/StorytellerStoriesGridView.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {
   ActivityType,
   GridView,
@@ -43,44 +41,26 @@ const StorytellerStoriesGridView = ({
       return;
     }
     // This method creates a new Storyteller grid, replacing the div with the id generated above
-    // For more information on creating and configuring Storyteller lists, please see
-    // https://www.getstoryteller.com/documentation/web/storyteller-grid-view
-    //
-    // The grid will display stories from the categories contained in the categories array
-    // For more information on stories and categories, please see
-    // https://www.getstoryteller.com/user-guide/stories-and-scheduling/categories
     storyGrid.current = new GridView(id, categories);
     storyGrid.current.displayLimit = displayLimit;
     storyGrid.current.theme = new UiTheme({
       light: FOUR_COLUMNS_LAYOUT,
       dark: FOUR_COLUMNS_LAYOUT,
     });
-    // The Story Grid has a delegate object attached which allows your code
-    // to take actions based on events which happen inside the Storyteller SDK
-    // For more information on the various delegate callbacks, please see
-    // https://www.getstoryteller.com/documentation/web/storyteller-list-view-delegate
     storyGrid.current.delegate = {
-      // This callback allows you to specify the ad configuration for the grid.
-      //
-      // For more information on how to configure Ads for the Storyteller Web SDK
-      // please see https://www.getstoryteller.com/documentation/web/ads
       getAdConfig: () => {
         return {
           slot: '/33813572/qa-ads',
+          customTargeting: {  // Add the customTargeting property
+            key1: 'value1',
+            key2: 'value2',
+            // Add other custom targeting options as needed
+          }
         };
       },
-      // This callback is used to inform your code about actions which a user
-      // takes inside Storyteller. This example shows how the data from this
-      // event can be sent to Amplitude for analytics purposes (but, of course,
-      // you could use any analytics provider you wish).
-      // For more information on the events and associated data, please see:
-      // https://www.getstoryteller.com/documentation/web/analytics
       onUserActivityOccurred: (type: ActivityType, data: UserActivityData) => {
         logUserActivityToAmplitude(type, data);
       },
-      // This callback is used to inform your code when a user taps a share
-      // button on any story. This example shows how to modify the URL which is
-      // shared before the share sheet is presented to the user.
       onShareButtonClicked: (text: string, title: string, url: string) => {
         const shareUrl = new URL(url);
         shareUrl.searchParams.append('utm_source', 'storyteller');
@@ -93,13 +73,7 @@ const StorytellerStoriesGridView = ({
         });
       },
     };
-  }, [
-    id,
-    categories,
-    displayLimit,
-    logUserActivityToAmplitude,
-    isStorytellerInitialized,
-  ]);
+  }, [id, categories, displayLimit, logUserActivityToAmplitude, isStorytellerInitialized]);
 
   return (
     <>
